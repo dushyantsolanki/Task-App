b954ff15f50186c2973f8626049287c26fd19fcd
dushyantsolanki <fullmoj70@gmail.com>
2025-06-03 21:57:59 +0530

    Deployement test.

---
+++ frontend/firebaseConfig.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onMessage } from 'firebase/messaging';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_ID,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENTID,
+};
+
+console.log('firebase config ::: ', firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+
+export const messaging = getMessaging(app);
+
+export const onMessageListener = () => {
+  return new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      console.log('message payload :>>', payload);
+      resolve(payload);
+    });
+  });
+};
