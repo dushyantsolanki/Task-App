ad98ec26a1dce27a18d6830e6f29250f6a37583e
Dushyant Solanki <112596904+dushyantsolanki@users.noreply.github.com>
2025-07-14 14:42:57 +0530

    Update utils.js

--- backend/utils/utils.js
+++ backend/utils/utils.js
@@ -6,13 +6,13 @@ export const generateAccessToken = (user) => {
     { userId: user._id, email: user.email, role: user.role },
     process.env.JWT_SECRET,
     {
-      expiresIn: '15m',
+      expiresIn: '7d',
     },
   );
 };
 
 export const generateRefreshToken = (user) => {
-  return jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
+  return jwt.sign({ userId: user._id }, process.env.JWT_REFRESH_SECRET, { expiresIn: '15d' });
 };
 
 export const generateOTP = () => {
