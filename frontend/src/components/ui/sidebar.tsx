44db1de133b4742a17958b119e25ccfdbeef6f09
dushyantsolanki <fullmoj70@gmail.com>
2025-09-05 13:38:59 +0530

    FIXED a sidebar bug.

--- frontend/src/components/ui/sidebar.tsx
+++ frontend/src/components/ui/sidebar.tsx
@@ -169,7 +169,7 @@ function Sidebar({
       <div
         data-slot="sidebar"
         className={cn(
-          "bg-sidebar text-sidebar-foreground flex min-h-screen w-(--sidebar-width) flex-col",
+          "bg-sidebar text-sidebar-foreground flex h-full w-(--sidebar-width) flex-col",
           className
         )}
         {...props}
