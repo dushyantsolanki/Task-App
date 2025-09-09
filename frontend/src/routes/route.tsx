1bcf7a2596f061e63c879adc83df3cc937a1e769
dushyantsolanki <fullmoj70@gmail.com>
2025-09-09 19:03:21 +0530

    Fixed a error in dynamic imports.

--- frontend/src/routes/route.tsx
+++ frontend/src/routes/route.tsx
@@ -8,12 +8,12 @@ import { Search } from '@/pages/Search';
 import { Dashboard } from '@/components/app/components/Dashboard';
 import DashboardLayout from '@/layout/DashboardLayout';
 import Task from '@/components/app/components/Task';
+import Lead from '@/pages/Lead';
 
 const LoginForm = lazy(() => import('@/form/LoginForm'));
 const RegisterForm = lazy(() => import('@/form/RegisterForm'));
 const OtpVerifyForm = lazy(() => import('@/form/OtpVerifyForm'));
 const Porfile = lazy(() => import('@/pages/Profile'));
-const Lead = lazy(() => import('@/pages/Lead'));
 const Template = lazy(() => import('@/pages/Template'));
 
 type RouteChild = {
