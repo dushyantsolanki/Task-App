b8f49201c9782f55b6970c94e7b686ef08ea7d2f
dushyantsolanki <fullmoj70@gmail.com>
2025-11-11 12:21:10 +0530

    FIXED a google cloude credentails path.

--- backend/configs/googlecloud.config.js
+++ backend/configs/googlecloud.config.js
@@ -6,8 +6,9 @@ import { google } from 'googleapis';
 
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 
-const TOKEN_PATH = path.join(process.cwd(), '..', 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), '..', 'credentials.json');
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+console.log(CREDENTIALS_PATH, TOKEN_PATH);
 
 async function loadSavedCredentialsIfExist() {
   try {
